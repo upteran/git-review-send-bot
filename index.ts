aa306edb815b0937a26a31a479fdb055c652ca0b
au_tereshkin <au_tereshkin@bingo-boom.com>
2023-03-05 12:19:56 +0400

    add: server

--- index.ts
+++ index.ts
@@ -1,10 +1,27 @@
 import Telegraf from 'telegraf';
+import http from 'http';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
 import { tgConfig } from './app/config/telegram';
 import { addBotCommands } from './app/helpers/addBotCommands';
 import { groupCommands } from './app/controllers/groupController';
 import { reviewCommands } from './app/controllers/reviewController';
+
+function runServer() {
+  const host = process.env.HOST || '0.0.0.0';
+  const port = process.env.PORT || 8080;
+
+  const requestListener = function (req: any, res: any) {
+    res.writeHead(200);
+    res.end('Ok');
+  };
+
+  const server = http.createServer(requestListener);
+  // @ts-ignore
+  server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+  });
+}
 
 const { apiKey } = tgConfig;
 
@@ -20,3 +37,4 @@ export function initBot(): void {
 }
 
 initBot();
+runServer();
